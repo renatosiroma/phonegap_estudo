51b79c3d6bea675efd5c3bf58664646a42e68025
renatosiroma <renato.siroma@gmail.com>
2014-09-25 15:32:31 -0300

    Inicio de Layout Dinamico

---
+++ js/acelerometro.js
@@ -0,0 +1,55 @@
+var watchID;
+
+		$("#accelerometer").bind("pageshow",function(){
+
+		  if (navigator.accelerometer) {
+
+		 	watchID = navigator.accelerometer.watchAcceleration(accelerometerSuccess, accelerometerError, { frequency: 100 });
+
+		  } else {
+
+			accelerometerError('navigator.accelerometer not supported');
+			window.addEventListener("devicemotion",devicemotionHandler,true);
+		}
+
+
+		  $("#accelerometer").bind("pagebeforehide",function(){
+
+			    if (watchID) {
+			      navigator.accelerometer.clearWatch(watchID);
+			    } else {
+			      window.removeEventListener("devicemotion",devicemotionHandler,true);
+			    }
+		    
+			})
+		});
+
+		function devicemotionHandler(event){
+			accelerometerSuccess(event.accelerationIncludingGravity);
+		}
+
+		function accelerometerSuccess(acceleration) {
+			$("#accelerometerStatus").html('X: ' + acceleration.x + '<br />' +
+			              'Y: ' + acceleration.y + '<br />' +
+			              'Z: ' + acceleration.z);
+			              
+			var current = $("#box").position();
+
+			$("#box").css({
+			  left: checkValue(current.left - acceleration.x, 0, 280) + "px",
+			  top: checkValue(current.top + acceleration.y, 0, 290) + "px",
+			});
+		}
+
+		function checkValue(value,min,max){
+			if(value<min){
+			  value = min;
+			} else if(value>max){
+			  value = max;
+			}
+			return parseInt(value);
+		}
+
+		function accelerometerError(msg) {
+			$("#accelerometerStatus").html(typeof msg == 'string' ? msg : "failed");
+		}
---
+++ js/camera.js
@@ -0,0 +1,44 @@
+ 		var pictureSource;   // picture source
+	    var destinationType; // sets the format of returned value
+
+	    document.addEventListener("deviceready",onDeviceReady,false);
+
+	    function onDeviceReady() {
+
+	    	/* Camera */
+	        pictureSource=navigator.camera.PictureSourceType;
+	        destinationType=navigator.camera.DestinationType;
+
+	        /* Connections */
+	        setInterval(function(){
+	        	checkConnection()
+	        }, 3000);
+
+	    }
+
+	    function onPhotoDataSuccess(imageData) {
+	      var image = document.getElementById('smallImage');
+	      image.style.display = 'block';
+	      image.src = "data:image/jpeg;base64," + imageData;
+	    }
+
+	    function onPhotoURISuccess(imageURI) {
+	      var image = document.getElementById('smallImage');
+	      image.style.display = 'block';
+	      image.src = imageURI;
+	    }
+
+	    function capturePhoto() {
+	      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+	        destinationType: destinationType.DATA_URL });
+	    }
+
+	    function getPhoto(source) {
+	      navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
+	        destinationType: destinationType.FILE_URI,
+	        sourceType: source });
+	    }
+
+	    function onFail(message) {
+	      alert('Failed because: ' + message);
+	    }
